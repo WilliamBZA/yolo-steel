582fc921b700a9e9f28d66569c03f34d61ebe58c
WilliamBZA <william@williamb.net>
2014-02-19 13:59:28 +0200

    Added exercise list test

--- yolo-steel/yolo.Domain.Tests/WorkoutTests.cs
+++ yolo-steel/yolo.Domain.Tests/WorkoutTests.cs
@@ -55,5 +55,34 @@ namespace yolo.Domain.Tests
             var field = workoutRoot.GetType().GetField("_name", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(workoutRoot) as string;
             Assert.Equal("Create my mofo workout please", field);
         }
+
+        [Fact]
+        public void Replaying_A_WorkoutCreatedEvent_And_ExerciseAddedEvent_Sets_The_WorkoutName_And_Exercises_Correctly()
+        {
+            // Arrange
+            var events = new IDomainEvent<Workout> [] { 
+                new WorkoutCreatedEvent("Create my mofo workout please"),
+                new ExerciseAddedEvent("Skwaatz", 300, 0)
+            };
+            var workoutRoot = new Workout();
+
+            // Act
+            workoutRoot.ReplayFromEvents(events);
+
+            // Assert
+            // OMG, am I really forced to use reflection to test this? Surely there's another indicator that the state is valid without
+            // exposing a public property on the aggregate?
+            var exercises = workoutRoot.GetType().GetField("_exercises", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(workoutRoot) as List<Exercise>;
+            Assert.Equal(1, exercises.Count);
+
+            var instance = exercises.First();
+            var exerciseName = instance.GetType().GetField("_name", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(instance) as string;
+            var exerciseReps = (int)instance.GetType().GetField("_reps", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(instance);
+            var exerciseWeight = (decimal)instance.GetType().GetField("_prescribedWeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(instance);
+
+            Assert.Equal("Skwaatz", exerciseName);
+            Assert.Equal(300, exerciseReps);
+            Assert.Equal(0, exerciseWeight);
+        }
     }
 }
